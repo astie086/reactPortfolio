a24a305d39742c87a09b199ec86dfb01d7903356
astie086 <astie086@gmail.com>
2018-09-05 15:25:10 -0700

    left side about me

--- src/components/contact.js
+++ src/components/contact.js
@@ -6,7 +6,24 @@ class Contact extends Component {
     return (
       <div className="contact-body">
         <Grid className="contact-grid">
-          <Cell col={6}>half page</Cell>
+          <Cell col={6}>
+            <h2>Kendra Herrera </h2>
+            <img
+              src="https://cdn2.iconfinder.com/data/icons/avatar-2/512/Fred_man-512.png"
+              alt="avatar"
+              style={{ height: 250 }}
+            />
+            <p stype={{ width: "75%", margin: "auto", paddingTop: "1em" }}>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+          </Cell>
+
           <Cell col={6}>half page</Cell>
         </Grid>
       </div>
