146c71cac3f751cee48b0a48cd9e2fdfd158ef52
astie086 <astie086@gmail.com>
2018-09-05 13:25:47 -0700

    starting landing page

--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -1,10 +1,27 @@
 import React, { Component } from "react";
+import { Grid, Cell } from "react-mdl";
 
 class Landing extends Component {
   render() {
     return (
-      <div>
-        <h1> Landing Page</h1>
+      <div style={{ width: "100%", margin: "auto" }}>
+        <Grid className="landing-grid">
+          <Cell col={12}>
+            <img
+              src="https://upload.wikimedia.org/wikipedia/commons/a/a5/Maud_Stevens_Wagner.jpeg"
+              alt="avatar"
+              className="avatar-img"
+            />
+
+            <div className="banner-text">
+              <h1> Ink Charmer </h1>
+
+              <hr />
+
+              <p> Painter | Tattoo Artist | Glass Etcher</p>
+            </div>
+          </Cell>
+        </Grid>
       </div>
     );
   }
